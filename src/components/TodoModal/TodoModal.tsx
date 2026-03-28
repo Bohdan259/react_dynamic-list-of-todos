2d7c491eb56ed6763558f24897a4c26c2025fd52
Bohdan <bodiiiiic@gmail.com>
2026-03-28 13:49:43 +0000

    solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -10,80 +10,78 @@ type Props = {
   resetSelectedTodo: (todo: null) => void;
 };
 
-export const TodoModal = React.memo<Props> (({
-  showModal,
-  selectedTodo,
-  resetSelectedTodo,
-}) => {
-  const [user, setUser] = useState<User>();
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [loading, setLoading] = useState(true);
+export const TodoModal = React.memo<Props>(
+  ({ showModal, selectedTodo, resetSelectedTodo }) => {
+    const [user, setUser] = useState<User>();
+    const [errorMessage, setErrorMessage] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (selectedTodo?.userId) {
-      getUser(selectedTodo.userId)
-        .then(setUser)
-        .catch(() => setErrorMessage(true))
-        .finally(() => setLoading(false));
-    }
+    useEffect(() => {
+      if (selectedTodo?.userId) {
+        getUser(selectedTodo.userId)
+          .then(setUser)
+          .catch(() => setErrorMessage(true))
+          .finally(() => setLoading(false));
+      }
 
-    return;
-  }, [selectedTodo]);
+      return;
+    }, [selectedTodo]);
 
-  const handleButtonCloseModal = useCallback(() => {
-    showModal(false);
-    resetSelectedTodo(null);
-  },[])
+    const handleButtonCloseModal = useCallback(() => {
+      showModal(false);
+      resetSelectedTodo(null);
+    }, []);
 
-  return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+    return (
+      <div className="modal is-active" data-cy="modal">
+        <div className="modal-background" />
 
-      {loading && <Loader />}
-      {errorMessage && (
-        <p style={{ color: 'red', fontSize: '22px' }}>
-          Opps something wrong, please try again later!!!
-        </p>
-      )}
-      {!loading && !errorMessage && (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #{selectedTodo?.id}
+        {loading && <Loader />}
+        {errorMessage && (
+          <p style={{ color: 'red', fontSize: '22px' }}>
+            Opps something wrong, please try again later!!!
+          </p>
+        )}
+        {!loading && !errorMessage && (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                Todo #{selectedTodo?.id}
+              </div>
+
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={handleButtonCloseModal}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {selectedTodo?.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {selectedTodo?.completed ? (
+                  <strong className="has-text-success">Done</strong>
+                ) : (
+                  <strong className="has-text-danger">Planned</strong>
+                )}
+
+                {' by '}
+
+                <a href={`mailto:${user?.email}`}>{user?.name}</a>
+              </p>
             </div>
-
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-              onClick={handleButtonCloseModal}
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              {selectedTodo?.title}
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {selectedTodo?.completed ? (
-                <strong className="has-text-success">Done</strong>
-              ) : (
-                <strong className="has-text-danger">Planned</strong>
-              )}
-
-              {' by '}
-
-              <a href={`mailto:${user?.email}`}>{user?.name}</a>
-            </p>
           </div>
-        </div>
-      )}
-    </div>
-  );
-});
+        )}
+      </div>
+    );
+  },
+);
 
-TodoModal.displayName = 'TodoModal'
+TodoModal.displayName = 'TodoModal';
